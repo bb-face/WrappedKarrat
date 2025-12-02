bef736e4039764c21b7efaccff803d980fcb5d50
stefano <stefano1990@gmail.com>
2025-12-02 11:22:59 +0700

    Fix build errors

--- src/WKARRAT.sol
+++ src/WKARRAT.sol
@@ -12,28 +12,23 @@ contract WKARRAT is IWKARRAT {
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-    event Deposit(address indexed account, uint256 amount);
-    event Withdrawal(address indexed account, uint256 amount);
-
-    /// @inheritdoc IERC20
     function deposit() public payable {
         balanceOf[msg.sender] += msg.value;
         emit Deposit(msg.sender, msg.value);
         emit Transfer(address(0), msg.sender, msg.value);
     }
 
-    /// @inheritdoc IERC20
     function withdraw(uint256 amount) public {
-        require(balanceOf[msg.sender] >= amount, "WKARRAT: insufficient balance");
+        require(
+            balanceOf[msg.sender] >= amount,
+            "WKARRAT: insufficient balance"
+        );
         balanceOf[msg.sender] -= amount;
         payable(msg.sender).transfer(amount);
         emit Withdrawal(msg.sender, amount);
         emit Transfer(msg.sender, address(0), amount);
     }
 
-    /// @inheritdoc IERC20
     receive() external payable {
         deposit();
     }
@@ -46,7 +41,10 @@ contract WKARRAT is IWKARRAT {
     /// @inheritdoc IERC20
     function transfer(address to, uint256 value) public returns (bool) {
         require(to != address(0), "WKARRAT: transfer to zero address");
-        require(balanceOf[msg.sender] >= value, "WKARRAT: insufficient balance");
+        require(
+            balanceOf[msg.sender] >= value,
+            "WKARRAT: insufficient balance"
+        );
 
         balanceOf[msg.sender] -= value;
         balanceOf[to] += value;
@@ -62,10 +60,17 @@ contract WKARRAT is IWKARRAT {
     }
 
     /// @inheritdoc IERC20
-    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) public returns (bool) {
         require(to != address(0), "WKARRAT: transfer to zero address");
         require(balanceOf[from] >= value, "WKARRAT: insufficient balance");
-        require(allowance[from][msg.sender] >= value, "WKARRAT: insufficient allowance");
+        require(
+            allowance[from][msg.sender] >= value,
+            "WKARRAT: insufficient allowance"
+        );
 
         balanceOf[from] -= value;
         allowance[from][msg.sender] -= value;
